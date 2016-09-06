a59ce8f984f0b8c94d09475eb3343c46d16af967
unknown <5093686@U702422.univali.br>
2016-09-06 20:06:06 -0300

    Atualizacao Ocamento 1

--- modules/orcamentos/partials/orcamentos.php
+++ modules/orcamentos/partials/orcamentos.php
@@ -2,10 +2,16 @@
 <div class="col s12 form">        
 
     <div class="card-panel">
-        <span class="card-title"><h5>
-                <i class="material-icons prefix">assignment</i>
-                Orçamento</h5></span>
-        
+        <span class="card-title"><h5><i class="material-icons prefix">assignment</i>Orçamento</h5></span>
+
+
+     
+
+
+    </div>
+
+    <div class="card-panel">
+        <span class="card-title"><h5><i class="material-icons prefix"></i>Produtos</h5></span>
         <div id="listas">
             <div class="row">
                 <div class="input-field col s12 m6 16">
@@ -19,17 +25,19 @@
                 <button href="#" class="btn remover_campo" style="margin-top: 1rem;"><i class="material-icons prefix">clear</i></button>
             </div>
         </div>
-        <button class="btn material-icons prefix add_a" type="button" id="add_a">add</button>
-               
-    </div>
-     <div class="row fim-form">
-            <div class="col s12 right-align">
-                <input type="hidden" name="id" value="" />
-                <input type="hidden" name="acao" value="" />                   
-                <button class="btn waves-effect waves-light" type="submit" name="action"><i class="material-icons left">send</i>Enviar Oçamento</button>
-            </div>                
+        <button class="btn prefix add_a" type="button" id="add_a">Incluir Produtos</button>
 
-        </div>
-    
+
+
+    </div>
+    <div class="row fim-form">
+        <div class="col s12 right-align">
+            <input type="hidden" name="id" value="" />
+            <input type="hidden" name="acao" value="" />                   
+            <button class="btn waves-effect waves-light" type="submit" name="action"><i class="material-icons left">send</i>Enviar Oçamento</button>
+        </div>                
+
+    </div>
+
 </div>
 
