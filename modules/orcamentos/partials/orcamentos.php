0a1a99a9bf59c4392dd8e71cf44bda63d3aefe07
igsrc <ivan.galvagno@gmail.com>
2016-09-05 21:38:15 -0300

    ocamento e produtos nao ta finalizado

--- modules/orcamentos/partials/orcamentos.php
+++ modules/orcamentos/partials/orcamentos.php
@@ -5,25 +5,29 @@
         <span class="card-title"><h5>
                 <i class="material-icons prefix">assignment</i>
                 Orçamento</h5></span>
-        <div class="row">
-            <div class="input-field col s12 m6 l6" >
-                <!--<i class="material-icons prefix">account_circle</i>-->
-                <input type="text" id="nomeProduto" value="" required/>
-                <label for="nome">Nome</label>
+        
+        <div id="listaOçamento">
+            <div class="row">
+                <div class="input-field col s12 m6 16">
+                    <input type="text" id="produtos[0]" value=""/>
+                    <label for="produtos[0]">Nome do Produto</label>
+                </div>
+                <div class="input-field col s12 m5 15">
+                    <input type="text" id="preço[0]" value="" class="preço" />
+                    <label for="preço[0]">Preço Unitário</label>
+                </div>
+                <button href="#" class="btn remover_campo" style="margin-top: 1rem;"><i class="material-icons prefix">clear</i></button>
             </div>
-            <div class="input-field col s12 m6 l6" >
-               <!--<i class="material-icons prefix">account_circle</i>-->
-                <input type="text" id="precoProduto" value="" required/>
-                <label for="nome">Código</label>
-            </div>
-            
         </div>
+        <button class="btn material-icons prefix add_field" type="button" id="add_field">add</button>
+        
+
 
         <div class="row fim-form">
             <div class="col s12 right-align">
                 <input type="hidden" name="id" value="" />
                 <input type="hidden" name="acao" value="" />                   
-                <button class="btn waves-effect waves-light" type="submit" name="action"><i class="material-icons left">send</i>Cadastrar Produto</button>
+                <button class="btn waves-effect waves-light" type="submit" name="action"><i class="material-icons left">send</i>Enviar Oçamento</button>
             </div>                
 
         </div>
