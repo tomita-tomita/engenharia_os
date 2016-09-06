67badc1065a43a18240f533bc0a55b834fabcc9b
igsrc <ivan.galvagno@gmail.com>
2016-09-05 21:21:32 -0300

    Ocamento

--- modules/orcamentos/partials/orcamentos.php
+++ modules/orcamentos/partials/orcamentos.php
@@ -1,55 +1,32 @@
+<script src="script.js" type="text/javascript"></script> 
+<div class="col s12 form">        
 
-<script src="script.js" type="text/javascript"></script>
-<div>    
-    <div class="col s12 form">
-        <div class="card-panel">
-            <form>
-                <div class="row">              
-                    <div class="input-field col s12 m6 l6" >
-                        <i class="material-icons prefix">account_circle</i>
-                        <input type="text" id="nome" value="" required/>
-                       <label for="nome">NOME*</label>
-                    </div>
-                    <div class="input-field col s12 m6 l6" >
-                        <i class="material-icons prefix">description</i>
-                        <input type="text" id="cpf" value=""  />
-                       <label for="cpf">CPF*</label>
-                    </div>
-                    <div class="input-field col s12 m6 l6" >
-                        <i class="material-icons prefix">place</i>
-                        <input type="text" id="endereco" value=""  />
-                       <label for="endereco">ENDEREÇO</label>
-                    </div>
-                    <div class="input-field col s12 m2 l2" >
-                        <i class="material-icons prefix">filter_1</i>
-                        <input type="text" id="numero" value=""  />
-                       <label for="numero">NÚMERO</label>
-                    </div>
-                    <div class="input-field col s12 m4 l4" >
-                        <i class="material-icons prefix">local_offer</i>
-                        <input type="text" id="comple" value=""  />
-                       <label for="comple">COMPLEMENTO</label>
-                    </div>
-                    <div class="input-field col s12 m6 l6" >
-                        <i class="material-icons prefix"></i>
-                        <input id="email" type="email" class="validate" value=""  />
-                       <label for="email" data-error="Email inválido" data-success="right">EMAIL</label>
-                    </div>  
-                    <div class="input-field col s12 m6 l6" >
-                        <i class="material-icons prefix"></i>
-                        <input type="date" class="datepicker" id="nasci" value=""  />
-                       <label for="nasci">Aniversário</label>
-                    </div>                      
-                    <!--<input type="text" name="nome" style="width: 22.5em" placeholder="Nome" value="<?php echo $nome ?>"  />-->
-                </div>                      
-                <div class="row fim-form">
-                    <div class="col s12 right-align">
- 			<input type="hidden" name="id" value="" />
-			<input type="hidden" name="acao" value="" />                   
-                        <button class="btn-large waves-effect waves-light" type="submit" name="action"><i class="material-icons left">send</i>SALVAR</button>
-                    </div>
-                </div>
-            </form>
+    <div class="card-panel">
+        <span class="card-title"><h5>
+                <i class="material-icons prefix">assignment</i>
+                Orçamento</h5></span>
+        <div class="row">
+            <div class="input-field col s12 m6 l6" >
+                <!--<i class="material-icons prefix">account_circle</i>-->
+                <input type="text" id="nomeProduto" value="" required/>
+                <label for="nome">Nome</label>
+            </div>
+            <div class="input-field col s12 m6 l6" >
+               <!--<i class="material-icons prefix">account_circle</i>-->
+                <input type="text" id="precoProduto" value="" required/>
+                <label for="nome">Código</label>
+            </div>
+            
+        </div>
+
+        <div class="row fim-form">
+            <div class="col s12 right-align">
+                <input type="hidden" name="id" value="" />
+                <input type="hidden" name="acao" value="" />                   
+                <button class="btn waves-effect waves-light" type="submit" name="action"><i class="material-icons left">send</i>Cadastrar Produto</button>
+            </div>                
+
         </div>
     </div>
 </div>
+
--- modules/produtos/partials/produtos.php
+++ modules/produtos/partials/produtos.php
@@ -1,40 +1,59 @@
-
-<script src="script.js" type="text/javascript"></script>
-<div>    
-    <div class="col s12 form">
+<script src="script.js" type="text/javascript"></script> 
+<div class="col s12 form">        
+                     
         <div class="card-panel">
-            <form>
-                <div class="row">              
-                    <div class="input-field col s12 m6 l6" >
-                        <i class="material-icons prefix">account_circle</i>
-                        <input type="text" id="nome" value="" required/>
-                       <label for="nome">NOME*</label>
-                    </div>
-                    <div class="input-field col s12 m6 l6" >
-                        <i class="material-icons prefix">description</i>
-                        <input type="text" id="cpf" value=""  />
-                       <label for="cpf">PREÇO</label>
-                    </div>          
-                    <div class="input-field col s12 m6 l6" >
-                        <i class="material-icons prefix"></i>
-                        <input id="email" type="email" class="validate" value=""  />
-                       <label for="email" data-error="Email inválido" data-success="right">EMAIL</label>
-                    </div>  
-                    <div class="input-field col s12 m6 l6" >
-                        <i class="material-icons prefix"></i>
-                        <input type="date" class="datepicker" id="nasci" value=""  />
-                       <label for="nasci">Aniversário</label>
-                    </div>                      
-                    <!--<input type="text" name="nome" style="width: 22.5em" placeholder="Nome" value="<?php echo $nome ?>"  />-->
-                </div>                      
-                <div class="row fim-form">
-                    <div class="col s12 right-align">
- 			<input type="hidden" name="id" value="" />
-			<input type="hidden" name="acao" value="" />                   
-                        <button class="btn-large waves-effect waves-light" type="submit" name="action"><i class="material-icons left">send</i>SALVAR</button>
-                    </div>
+            <span class="card-title"><h5>
+                    <i class="material-icons prefix">shopping_basket</i>
+                    Dados do Produto</h5></span>
+            <div class="row">
+                <div class="input-field col s12 m6 l6" >
+                    <!--<i class="material-icons prefix">account_circle</i>-->
+                    <input type="text" id="nomeProduto" value="" required/>
+                    <label for="nome">Nome</label>
                 </div>
-            </form>
+                <div class="input-field col s12 m6 l6" >
+                   <!--<i class="material-icons prefix">account_circle</i>-->
+                    <input type="text" id="precoProduto" value="" required/>
+                    <label for="nome">Código</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12 m6 l6" >
+                    <!--<i class="material-icons prefix">account_circle</i>-->
+                    <input type="number" id="precoProduto" value="" required/>
+                    <label for="nome">Preço Unitário</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea id="descricaoProduto" class="materialize-textarea" length="120"></textarea>
+                    <label for="textarea1">Descrição Produto</label>
+                </div>
+            </div>
+
+        </div>                      
+        <div class="row fim-form">
+            <div class="col s12 right-align">
+                <input type="hidden" name="id" value="" />
+                <input type="hidden" name="acao" value="" />                   
+                <button class="btn waves-effect waves-light" type="submit" name="action"><i class="material-icons left">send</i>Cadastrar Produto</button>
+            </div>                
+     
+</div>
+</div>
+
+<!-- colocar no produto final  / trocar a cor porque essa é zuada !-->
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+            </div>
         </div>
     </div>
-</div>
+    <div class="footer-copyright">
+        <div class="container">
+            © 2016 Copyright Text
+        </div>
+    </div>
+</footer>
